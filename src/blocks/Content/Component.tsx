50c44a26e1c553e01ab94d05a53da5e17cedb6dc
ahadtamim <mahmudul.tamim2020@gmail.com>
2026-04-18 21:56:44 +0600

    fix: neutralize ContentBlock types

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -1,38 +1,30 @@
 import { cn } from '@/utilities/ui'
 import React from 'react'
 import RichText from '@/components/RichText'
-
-import type { ContentBlock as ContentBlockProps } from '@/payload-types'
-
 import { CMSLink } from '../../components/Link'
 
-export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
+export const ContentBlock: React.FC<any> = (props) => {
   const { columns } = props
-
-  const colsSpanClasses = {
-    full: '12',
-    half: '6',
-    oneThird: '4',
-    twoThirds: '8',
-  }
 
   return (
     <div className="container my-16">
       <div className="grid grid-cols-4 lg:grid-cols-12 gap-y-8 gap-x-16">
         {columns &&
           columns.length > 0 &&
-          columns.map((col, index) => {
+          columns.map((col: any, index: number) => {
             const { enableLink, link, richText, size } = col
 
             return (
               <div
-                className={cn(`col-span-4 lg:col-span-${colsSpanClasses[size!]}`, {
-                  'md:col-span-2': size !== 'full',
+                className={cn('col-span-4', {
+                  'lg:col-span-12': size === 'full',
+                  'lg:col-span-8': size === 'twoThirds',
+                  'lg:col-span-6': size === 'half',
+                  'lg:col-span-4': size === 'oneThird',
                 })}
                 key={index}
               >
                 {richText && <RichText data={richText} enableGutter={false} />}
-
                 {enableLink && <CMSLink {...link} />}
               </div>
             )
@@ -41,3 +33,5 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
     </div>
   )
 }
+
+export default ContentBlock
