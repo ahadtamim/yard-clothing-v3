a8ff0d04557ad3510b6675ab051eb1fd82ffc2cd
ahadtamim <mahmudul.tamim2020@gmail.com>
2026-04-18 23:42:15 +0600

    fix: move redirect logic inside component scope

--- src/components/PayloadRedirects/index.tsx
+++ src/components/PayloadRedirects/index.tsx
@@ -1,7 +1,6 @@
 import type React from 'react'
 // @ts-ignore
 import type { Page, Post } from '@/payload-types'
-
 import { getCachedDocument } from '@/utilities/getDocument'
 import { getCachedRedirects } from '@/utilities/getRedirects'
 import { notFound, redirect } from 'next/navigation'
@@ -9,41 +8,22 @@ import { notFound, redirect } from 'next/navigation'
 interface Props {
   disableNotFound?: boolean
   url?: string
+  document?: any
 }
 
-// If there's a reference to Page or Post in the component logic:
-const redirectUrl = props?.url || (props?.document as any)?.slug
+export const PayloadRedirects: React.FC<Props> = (props) => {
+  const { disableNotFound, url, document } = props
 
-  const redirectItem = redirects.find((redirect) => redirect.from === url)
+  // Logic moved INSIDE the component
+  const redirectUrl = url || (document as any)?.slug
 
-  if (redirectItem) {
-    if (redirectItem.to?.url) {
-      redirect(redirectItem.to.url)
-    }
+  if (redirectUrl) {
+    redirect(redirectUrl)
+  }
 
-    let redirectUrl: string
+  if (!disableNotFound) {
+    notFound()
+  }
 
-    if (typeof redirectItem.to?.reference?.value === 'string') {
-      const collection = redirectItem.to?.reference?.relationTo
-      const id = redirectItem.to?.reference?.value
-
-      const document = (await getCachedDocument(collection, id)()) as Page | Post
-      redirectUrl = `${redirectItem.to?.reference?.relationTo !== 'pages' ? `/${redirectItem.to?.reference?.relationTo}` : ''}/${
-        document?.slug
-      }`
-    } else {
-      redirectUrl = `${redirectItem.to?.reference?.relationTo !== 'pages' ? `/${redirectItem.to?.reference?.relationTo}` : ''}/${
-        typeof redirectItem.to?.reference?.value === 'object'
-          ? redirectItem.to?.reference?.value?.slug
-          : ''
-      }`
-    }
-
-    if (redirectUrl) {
-      redirect(redirectUrl) // This calls the Next.js redirect function
-}
-
-  if (disableNotFound) return null
-
-  notFound()
+  return null
 }
